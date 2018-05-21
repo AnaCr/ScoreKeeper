d31af1716cc210adf2f973e9f73e9f15ad7d391e
Ana Cruz <anlaucruz@gmail.com>
2018-05-20 21:19:26 -0500

    Added editable player names.

--- app/src/main/java/com/example/anala/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/anala/scorekeeper/MainActivity.java
@@ -1,19 +1,16 @@
 package com.example.anala.scorekeeper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    int scorePOne = 0;
-    int scorePTwo = 0;
-    int hole = 1;
-    String gameOver = "false";
-
-
+    public final static String EXTRA_MESSAGE1 = "com.example.anala.scorekeeper.MESSAGE1";
+    public final static String EXTRA_MESSAGE2 = "com.example.anala.scorekeeper.MESSAGE2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,190 +18,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /** displays the score for Player One */
-    public void displayScorePOne (int score){
-        TextView scoreView = findViewById(R.id.playerOneScore);
-        scoreView.setText(String.valueOf(score));
+    /** Called when the user taps the Play! button */
+    /** Credit: https://developer.android.com/training/basics/firstapp/starting-activity#java */
+    public void beginScoreBoard(View view) {
+        Intent intent = new Intent(this, ScoreBoardActivity.class);
+        EditText player1editText = findViewById(R.id.player1);
+        EditText player2editText = findViewById(R.id.player2);
+        String player1name = player1editText.getText().toString();
+        String player2name = player2editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE1, player1name);
+        intent.putExtra(EXTRA_MESSAGE2, player2name);
+        startActivity(intent);
     }
 
-    /** displays the score for Player Two */
-    public void displayScorePTwo (int score){
-        TextView scoreView = findViewById(R.id.playerTwoScore);
-        scoreView.setText(String.valueOf(score));
-    }
 
-    /** displays the hole number */
-    public void displayHoleNum (int holeNum){
-        TextView holeNumView = findViewById(R.id.holeTxtView);
-        holeNumView.setText("Hole: " + String.valueOf(holeNum) );
-    }
-
-    /** displays result P1 */
-    public void displayResultP1 (String result){
-        TextView resultP1 = findViewById(R.id.resultP1);
-        resultP1.setText(result);
-    }
-
-    /** displays result P2 */
-    public void displayResultP2 (String result){
-        TextView resultP1 = findViewById(R.id.resultP2);
-        resultP1.setText(result);
-    }
-
-    /** Ends the game */
-    public void endGame (String gameOver){
-        if (gameOver.equals("true") ){
-            Toast.makeText(getApplicationContext(), "Game Over!", Toast.LENGTH_SHORT).show();
-            if (scorePOne < scorePTwo){
-                displayResultP1("Winner!");
-                displayResultP2("Loser!");
-            }
-            if (scorePTwo < scorePOne){
-                displayResultP1("Loser!");
-                displayResultP2("Winner!");
-            }
-            if (scorePOne == scorePTwo){
-                displayResultP1("Tie!");
-                displayResultP2("Tie!");
-            }
-        }
-    }
-
-    /** adds 1 to Player 1 score */
-    public void holeInOneP1 (View v){
-        if (gameOver.equals("false")){
-            scorePOne = scorePOne + 1;
-            displayScorePOne(scorePOne);
-        }
-    }
-
-    /** adds 1 to Player 2 score */
-    public void holeInOneP2 (View v){
-        if (hole < 18) {
-            scorePTwo = scorePTwo + 1;
-            hole = hole + 1;
-            displayScorePTwo(scorePTwo);
-            displayHoleNum(hole);
-        }
-        else if (hole == 18){
-            scorePTwo = scorePTwo + 1;
-            hole = hole + 1;
-            displayScorePTwo(scorePTwo);
-            gameOver = "true";
-            endGame(gameOver);
-        }
-    }
-
-    /** adds 2 to Player 1 score */
-    public void plusTwoP1 (View v){
-        if (gameOver.equals("false")){
-            scorePOne = scorePOne + 2;
-            displayScorePOne(scorePOne);
-        }
-    }
-
-    /** adds 2 to Player 2 score */
-    public void plusTwoP2 (View v){
-        if (hole < 18) {
-            scorePTwo = scorePTwo + 2;
-            hole = hole + 1;
-            displayScorePTwo(scorePTwo);
-            displayHoleNum(hole);
-        }
-        else if (hole == 18){
-            scorePTwo = scorePTwo + 2;
-            hole = hole + 1;
-            displayScorePTwo(scorePTwo);
-            gameOver = "true";
-            endGame(gameOver);
-        }
-    }
-
-    /** adds 3 to Player 1 score */
-    public void plusThreeP1 (View v){
-        if (gameOver.equals("false")){
-            scorePOne = scorePOne + 3;
-            displayScorePOne(scorePOne);
-        }
-    }
-
-    /** adds 3 to Player 2 score */
-    public void plusThreeP2 (View v){
-        if (hole < 18) {
-            scorePTwo = scorePTwo + 3;
-            hole = hole + 1;
-            displayScorePTwo(scorePTwo);
-            displayHoleNum(hole);
-        }
-        else if (hole == 18){
-            scorePTwo = scorePTwo + 3;
-            hole = hole + 1;
-            displayScorePTwo(scorePTwo);
-            gameOver = "true";
-            endGame(gameOver);
-        }
-    }
-
-    /** adds 4 to Player 1 score */
-    public void plusFourP1 (View v){
-        if (gameOver.equals("false")){
-            scorePOne = scorePOne + 4;
-            displayScorePOne(scorePOne);
-        }
-    }
-
-    /** adds 4 to Player 2 score */
-    public void plusFourP2 (View v){
-        if (hole < 18) {
-            scorePTwo = scorePTwo + 4;
-            hole = hole + 1;
-            displayScorePTwo(scorePTwo);
-            displayHoleNum(hole);
-        }
-        else if (hole == 18){
-            scorePTwo = scorePTwo + 4;
-            hole = hole + 1;
-            displayScorePTwo(scorePTwo);
-            gameOver = "true";
-            endGame(gameOver);
-        }
-    }
-
-    /** adds 5 to Player 1 score */
-    public void plusFiveP1 (View v){
-        if (gameOver.equals("false")){
-            scorePOne = scorePOne + 5;
-            displayScorePOne(scorePOne);
-        }
-    }
-
-    /** adds 5 to Player 2 score */
-    public void plusFiveP2 (View v){
-        if (hole < 18) {
-            scorePTwo = scorePTwo + 5;
-            hole = hole + 1;
-            displayScorePTwo(scorePTwo);
-            displayHoleNum(hole);
-        }
-        else if (hole == 18){
-            scorePTwo = scorePTwo + 5;
-            hole = hole + 1;
-            displayScorePTwo(scorePTwo);
-            gameOver = "true";
-            endGame(gameOver);
-        }
-    }
-
-    /** Resets the game */
-    public void resetGame (View v){
-        hole = 1;
-        scorePOne = 0;
-        scorePTwo = 0;
-        gameOver = "false";
-        displayScorePOne(scorePOne);
-        displayScorePTwo(scorePTwo);
-        displayHoleNum(hole);
-        displayResultP1(" ");
-        displayResultP2(" ");
-    }
 }
---
+++ app/src/main/java/com/example/anala/scorekeeper/ScoreBoardActivity.java
@@ -0,0 +1,228 @@
+package com.example.anala.scorekeeper;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ScoreBoardActivity extends AppCompatActivity {
+
+    int scorePOne = 0;
+    int scorePTwo = 0;
+    int hole = 1;
+    int turn = 1;
+    String gameOver = "false";
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_board);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        /** Get the name for Player 1 from MainActivity */
+        Intent intent = getIntent();
+        String p1name = intent.getStringExtra(MainActivity.EXTRA_MESSAGE1);
+        String p2name = intent.getStringExtra(MainActivity.EXTRA_MESSAGE2);
+
+        TextView player1textView = findViewById(R.id.playerOne);
+        player1textView.setText(p1name);
+
+        TextView player2textView = findViewById(R.id.playerTwo);
+        player2textView.setText(p2name);
+    }
+
+    /** displays the score for Player One */
+    public void displayScorePOne (int score){
+        TextView scoreView = findViewById(R.id.playerOneScore);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /** displays the score for Player Two */
+    public void displayScorePTwo (int score){
+        TextView scoreView = findViewById(R.id.playerTwoScore);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /** displays the hole number */
+    public void displayHoleNum (int holeNum){
+        TextView holeNumView = findViewById(R.id.holeTxtView);
+        holeNumView.setText("Hole: " + String.valueOf(holeNum) );
+    }
+
+    /** displays result P1 */
+    public void displayResultP1 (String result){
+        TextView resultP1 = findViewById(R.id.resultP1);
+        resultP1.setText(result);
+    }
+
+    /** displays result P2 */
+    public void displayResultP2 (String result){
+        TextView resultP1 = findViewById(R.id.resultP2);
+        resultP1.setText(result);
+    }
+
+    /** Ends the game */
+    public void endGame (String gameOver){
+        if (gameOver.equals("true") ){
+            Toast.makeText(getApplicationContext(), "Game Over!", Toast.LENGTH_SHORT).show();
+            if (scorePOne < scorePTwo){
+                displayResultP1("Winner!");
+                displayResultP2("Loser!");
+            }
+            if (scorePTwo < scorePOne){
+                displayResultP1("Loser!");
+                displayResultP2("Winner!");
+            }
+            if (scorePOne == scorePTwo){
+                displayResultP1("Tie!");
+                displayResultP2("Tie!");
+            }
+        }
+    }
+
+    /** adds 1 to Player 1 score */
+    public void holeInOneP1 (View v){
+        if (gameOver.equals("false")){
+            scorePOne = scorePOne + 1;
+            displayScorePOne(scorePOne);
+        }
+    }
+
+    /** adds 1 to Player 2 score */
+    public void holeInOneP2 (View v){
+        if (hole < 18) {
+            scorePTwo = scorePTwo + 1;
+            hole = hole + 1;
+            displayScorePTwo(scorePTwo);
+            displayHoleNum(hole);
+        }
+        else if (hole == 18){
+            scorePTwo = scorePTwo + 1;
+            hole = hole + 1;
+            displayScorePTwo(scorePTwo);
+            gameOver = "true";
+            endGame(gameOver);
+        }
+    }
+
+    /** adds 2 to Player 1 score */
+    public void plusTwoP1 (View v){
+        if (gameOver.equals("false")){
+            scorePOne = scorePOne + 2;
+            displayScorePOne(scorePOne);
+        }
+    }
+
+    /** adds 2 to Player 2 score */
+    public void plusTwoP2 (View v){
+        if (hole < 18) {
+            scorePTwo = scorePTwo + 2;
+            hole = hole + 1;
+            displayScorePTwo(scorePTwo);
+            displayHoleNum(hole);
+        }
+        else if (hole == 18){
+            scorePTwo = scorePTwo + 2;
+            hole = hole + 1;
+            displayScorePTwo(scorePTwo);
+            gameOver = "true";
+            endGame(gameOver);
+        }
+    }
+
+    /** adds 3 to Player 1 score */
+    public void plusThreeP1 (View v){
+        if (gameOver.equals("false")){
+            scorePOne = scorePOne + 3;
+            displayScorePOne(scorePOne);
+        }
+    }
+
+    /** adds 3 to Player 2 score */
+    public void plusThreeP2 (View v){
+        if (hole < 18) {
+            scorePTwo = scorePTwo + 3;
+            hole = hole + 1;
+            displayScorePTwo(scorePTwo);
+            displayHoleNum(hole);
+        }
+        else if (hole == 18){
+            scorePTwo = scorePTwo + 3;
+            hole = hole + 1;
+            displayScorePTwo(scorePTwo);
+            gameOver = "true";
+            endGame(gameOver);
+        }
+    }
+
+    /** adds 4 to Player 1 score */
+    public void plusFourP1 (View v){
+        if (gameOver.equals("false")){
+            scorePOne = scorePOne + 4;
+            displayScorePOne(scorePOne);
+        }
+    }
+
+    /** adds 4 to Player 2 score */
+    public void plusFourP2 (View v){
+        if (hole < 18) {
+            scorePTwo = scorePTwo + 4;
+            hole = hole + 1;
+            displayScorePTwo(scorePTwo);
+            displayHoleNum(hole);
+        }
+        else if (hole == 18){
+            scorePTwo = scorePTwo + 4;
+            hole = hole + 1;
+            displayScorePTwo(scorePTwo);
+            gameOver = "true";
+            endGame(gameOver);
+        }
+    }
+
+    /** adds 5 to Player 1 score */
+    public void plusFiveP1 (View v){
+        if (gameOver.equals("false")){
+            scorePOne = scorePOne + 5;
+            displayScorePOne(scorePOne);
+        }
+    }
+
+    /** adds 5 to Player 2 score */
+    public void plusFiveP2 (View v){
+        if (hole < 18) {
+            scorePTwo = scorePTwo + 5;
+            hole = hole + 1;
+            displayScorePTwo(scorePTwo);
+            displayHoleNum(hole);
+        }
+        else if (hole == 18){
+            scorePTwo = scorePTwo + 5;
+            hole = hole + 1;
+            displayScorePTwo(scorePTwo);
+            gameOver = "true";
+            endGame(gameOver);
+        }
+    }
+
+    /** Resets the game */
+    public void resetGame (View v){
+        hole = 1;
+        scorePOne = 0;
+        scorePTwo = 0;
+        gameOver = "false";
+        displayScorePOne(scorePOne);
+        displayScorePTwo(scorePTwo);
+        displayHoleNum(hole);
+        displayResultP1(" ");
+        displayResultP2(" ");
+    }
+}
+
